7a411d7e7752cc9bb66dbb81917062246c4e62f4
root <root@torr20-station.>
2024-06-15 06:21:43 +0600

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -299,8 +299,8 @@ int main(int argc, char **argv)
     cout << "Accepted connection\n";
     // thread thrd(handle_client, newsockfd);
     // thrd.detach();
+    this_thread::sleep_for(chrono::milliseconds(2000));
     threads.emplace_back(handle_client, newsockfd);
-    // this_thread::sleep_for(chrono::milliseconds(2000));
   }
 
   close(server_fd);
